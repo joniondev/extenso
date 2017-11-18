581d9f435be15d50a1a3756a5f6bb73465d86ccd
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2017-11-18 15:40:49 -0200

    Padronização da nomenclatura dos projetos.

--- ejb/src/main/java/br/eti/romel/lounge/extenso/ejb/ExtensoBean.java
+++ ejb/src/main/java/br/eti/romel/lounge/extenso/ejb/ExtensoBean.java
@@ -6,10 +6,18 @@ package br.eti.romel.lounge.extenso.ejb;
 import br.eti.romel.lounge.extenso.*;
 import java.io.*;
 import java.math.*;
+import java.util.logging.*;
 import javax.ejb.*;
+import javax.inject.*;
 
+@Named
 @Stateless
 public class ExtensoBean implements Serializable, ExtensoBeanRemote {
+    private final double versao;
+
+    public ExtensoBean() {
+        this.versao = Math.random();
+    }
 
     /**
      *
@@ -21,6 +29,12 @@ public class ExtensoBean implements Serializable, ExtensoBeanRemote {
     public String extenso(BigDecimal valor) {
         Extenso extenso = new Extenso(valor);
 
-        return String.format("EJB(%s)", extenso.toString());
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ExtensoBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return String.format("%f-EJB(%s)", this.versao, extenso.toString());
     }
 }
---
+++ extenso-soap-cli-client/src/main/java/br/eti/romel/lounge/extenso/wsclient/ExtensoWSClient.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2013, 2017 Carlos Romel Pereira da Silva, carlos.romel@gmail.com
+ *
+ * Cliente (linha de comando) do ExtensoWS (prova de conceito).
+ */
+package br.eti.romel.lounge.extenso.wsclient;
+
+/**
+ *
+ * @author Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
+ */
+public class ExtensoWSClient {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Extenso: [%s]\n", extenso("999988887777666655554444333322221111"));
+    }
+
+    public static String extenso(String valor) {
+        ExtensoWS extensoWS = new ExtensoWS_Service().getExtensoWSPort();
+        return extensoWS.extenso(valor);
+    }
+}
